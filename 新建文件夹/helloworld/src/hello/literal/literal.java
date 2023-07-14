d4211adae7efa41d221383677af4efd9c7610ea2
gfdgrjgisn <139336080+gfdgrjgisn@users.noreply.github.com>
2023-07-14 17:43:05 +0800

    Add files via upload

---
+++ 新建文件夹/helloworld/src/hello/literal/literal.java
@@ -0,0 +1,12 @@
+package hello.literal;
+
+public class literal {
+    //字面量
+    public static void main(String[] args) {
+        System.out.println(573498);
+        System.out.println(73485.57);
+        System.out.println('s');
+        System.out.println("sjfi");
+
+    }
+}
---
+++ 新建文件夹/helloworld/src/hello/variable/variable.java
@@ -0,0 +1,17 @@
+package hello.variable;
+
+public class variable {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+
+        double score = 99.3;
+        System.out.println(score);
+        age = 23;
+        System.out.println(age);
+        age = age + 19;
+        System.out.println(age);
+
+
+    }
+}
