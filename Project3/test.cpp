2150804e86a3136c148ecdf66bb6ecdd9b19160b
gfdgrjgisn <139336080+gfdgrjgisn@users.noreply.github.com>
2023-07-26 18:57:59 +0800

    Add files via upload

---
+++ Project3/test.cpp
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+////常变量
+//const int sun = 10;
+////sun = 20;
+//int main()
+//{
+//	printf("sun = %d\n", sun);
+//	return 0;
+//}
+//#define MAX 10000
+//int main()
+//{
+//	int a = MAX;
+//	printf("a = %d\n", a);
+//	return 0;
+//}
+//enum Sex
+//{
+//	male,
+//	secret,
+//	female
+//};
+//
+//int main()
+//{
+//	enum Sex s = male;
+//	printf("%d\n", male);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	char a[] = "aba";
+//	char b[] = { 'a','b','c' };
+//	int length = strlen("aba");
+//	//printf("%d\n", length);
+//	printf("%d\n", strlen(a));
+//	/*printf("%s\n", a);
+//	printf("%s\n", b);*/
+//	return 0;
+//}
+//{
+//	int input = 0;
+//	printf("选择a或b");
+//	scanf("%d\n", &input);
+//	if (input == a)
+//		printf("恭喜");
+//	else
+//		printf("你好");
+//	return 0;
+//}
+//int axs(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	//int c = a + b;
+//	int c = axs(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+int main()
+{
+	char x[3] = { 'a' };
+	return 0;
+}
