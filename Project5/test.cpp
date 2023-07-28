abad1ddab73e4b9e0ab50014b9cef3075e31cd83
gfdgrjgisn <139336080+gfdgrjgisn@users.noreply.github.com>
2023-07-28 20:05:19 +0800

    Add files via upload

---
+++ Project5/test.cpp
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 534;
+//	int b = 634;
+//	scanf("%d %d", &a, & b);
+//	/*int a = 10;
+//	int b = 20;*/
+//	if (a > b)
+//		printf("%d\n", a);
+//	else
+//		printf("%d\n", b);
+//	return 0;
+//}
+//int ss(int a)
+//{
+//	int c = 0;
+//	static int b = 3;
+//	c ++;
+//	b += 2;
+//
+//	return (a + b + c);
+//}
+//
+//int main()
+//{
+//	int i;
+//	int a = 2;
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d\n", ss(2));
+//		printf("%d\n", ss(3));
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a % 5 == 0)
+//		printf("Yes");
+//	else
+//		printf("No");
+//	return 0;
+//}
+//int main()
+//{
+//	int age = 53;
+//	if (age < 18)
+//		printf("少年");
+//	else if (18 <= age && age < 30)
+//		printf("青年");
+//	else
+//		printf("老年");
+//	return 0;
+//}
+//int fs()
+//{
+//	return 54;
+//}
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("请输入密码");
+//	scanf("%s", password);
+//	printf("请输入密码");
+//	int a = 0;
+//	while (a = getchar() != '\n');
+//	{
+//		;
+//	}
+//	printf("请确认密码(Y\N)");
+//	int ch = getchar();
+//	if (ch == 'Y')
+//		printf("确认成功");
+//	else if (ch == 'N');
+//	printf("确认失败");
+//
+//
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
