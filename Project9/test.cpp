a21efb389522293230dbdc22aedbcb47a2e4a6e9
gfdgrjgisn <139336080+gfdgrjgisn@users.noreply.github.com>
2023-08-05 19:35:31 +0800

    Add files via upload

---
+++ Project9/test.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+//int main()
+//{
+//	char a[] = { 0 };
+//	system("shutdown -s -t 60");
+//	s:
+//	printf("一分钟后你的电脑将要关机，请输入取消，取消关机\n");;
+//	scanf("%s", a);
+//	if (strcmp(a, "我是猪") == 0)
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto s;
+//	}
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
