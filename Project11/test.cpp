833d97fe5e2e97dff5a13ee51c40abcdc6332236
gfdgrjgisn <139336080+gfdgrjgisn@users.noreply.github.com>
2023-08-07 19:24:28 +0800

    Add files via upload

---
+++ Project11/test.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+//void print(int a)
+//{
+//	if (a > 9)
+//	{
+//		print(a / 10);
+//	}
+//	printf("%d\n", a % 10);
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	print(a);
+//	return 0;
+//}
+//int ds(char* s)
+//{
+//	if (*s != '\0')
+//		return 1 + ds(s + 1);
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char a[] = { "abc" };
+//	printf("%d\n", ds(a));
+//	return 0;
+//}
+//int sum(int a)
+//{
+//	int b = 1;
+//	int c = 1;
+//	while (b <= a)
+//	{
+//		c = c * b;
+//		b = b + 1;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d\n", sum(a));
+//	return 0;
+//}
+//int fac(int a)
+//{
+//	if (a <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//		return fac(a - 1) + fac(a - 2);
+//	
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d\n", fac(a));
+//	return 0;
+//}
+int mian()
+{
+	return 0;
+}
