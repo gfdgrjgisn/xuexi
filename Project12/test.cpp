fa6a88388f7e1e0eadda1c6a1e3f1adf83edd8f7
gfdgrjgisn <139336080+gfdgrjgisn@users.noreply.github.com>
2023-08-08 20:38:18 +0800

    Add files via upload

---
+++ Project12/test.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WAENINGS
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void change(char* str)
+//{
+//	int left = 0;
+//	int right = my_strlen(str) - 1;
+//	while (left < right)
+//	{
+//		char tmp = str[left];
+//		str[left] = str[right];
+//		str[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdefg";
+//	change(arr);
+//	printf("%s", arr);
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
