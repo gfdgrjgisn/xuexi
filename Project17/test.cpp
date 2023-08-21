62c9ac680532210a747c7b3dfd414605ba78f628
gfdgrjgisn <139336080+gfdgrjgisn@users.noreply.github.com>
2023-08-21 11:13:31 +0800

    Add files via upload

---
+++ Project17/test.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+//int main()
+//{
+//	int c;
+//	c = getchar();
+//	while (c != EOF)
+//	{
+//		putchar(c);
+//		c = getchar();
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char c = getchar();
+//	putchar(c);
+//	return 0;
+//
+//}
+int main()
+{
+
+	return 0;
+}
